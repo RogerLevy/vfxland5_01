72e9ddb47dfc17c3734b9e5130426684f6199ce7
Roger <roger.levy@gmail.com>
2025-08-08 10:25:54 -0400

    Refactor Engineer initialization and dev tools and add VFX console echo
    - Move dev tools to engineer.vfx from build.vfx for consistency
    - Add supershow/main.vfx as project entry point
    - Clean up actor system initialization in stage.vfx
    - Echo console i/o to VFX console
    - Consolidate project structure around main.vfx pattern

--- dev.bat
+++ dev.bat
@@ -1,6 +1,2 @@
-@REM We call COLD here for the time being since we still rely on the VFX IDE
-@REM for output.
-
 set "PATH=%~dp0\bin;%~dp0\bin\dll\;%~dp0\bin\engineer\;%PATH%"
-@REM engineer.exe cd darkblue frigid
-engineer.exe ." Hello world" cr ." asdlfkajsdlfkasdjlfj" cr
+engineer.exe ." Welcome to VFX Land!"  cr  ld supershow
