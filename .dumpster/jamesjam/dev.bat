d7b16b1304c97b59dd124e4172f33a585af3aaa0
Roger <roger.levy@gmail.com>
2025-08-21 17:19:07 -0400

    Remove obsolete tooling files
    - Remove komodoprojects
    - Remove komodotools
    - Remove obsolete dev.bat's

--- .dumpster/jamesjam/dev.bat
+++
@@ -1,6 +0,0 @@
-@REM We call COLD here for the time being since we still rely on the VFX IDE
-@REM for output.
-
-@set PATH="%PATH%;engineer\bin\"
-cd ..\engineer
-bin\engineer.exe cwd %~dp0 cold
--- kvn/dev.bat
+++
@@ -1,9 +0,0 @@
-@REM We call COLD here for the time being since we still rely on the VFX IDE
-@REM for output.
-
-@set PATH="%PATH%;..\engineer\bin\"
-cd ..\engineer
-@REM bin\engineer.exe cold
-bin\engineer.exe cwd %~dp0 cold
-
-
