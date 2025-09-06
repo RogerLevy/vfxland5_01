cb671661b033db215ccf2939cfd0d70b99ed6557
Roger <roger.levy@gmail.com>
2025-09-06 14:25:13 -0400

    Engineer: Optimize release builds
    Disable run-time validations completely for zero overhead

--- engineer/turnkey.bat
+++ engineer/turnkey.bat
@@ -9,6 +9,6 @@ xcopy dat ..\rel\%1-debug\dat /i /s /q /y /e
 copy %~dp0\..\bin\*.dll ..\rel\%1
 copy %~dp0\..\bin\*.dll ..\rel\%1-debug
 
-call %~dp0\load-project.bat save-release ..\rel\%1\%1 save-debug ..\rel\%1-debug\%1-debug bye
+call %~dp0\load-project-release.bat save-release ..\rel\%1\%1 save-debug ..\rel\%1-debug\%1-debug bye
 cd ..\rel\%1
 %1.exe
