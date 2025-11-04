2c1629a1daca432af9a1761797ef87eeebf7cb17
Roger <roger.levy@gmail.com>
2025-11-03 23:01:39 -0500

    Rename several cjson wrapper words

--- ex/tiled/assets/scifi.tsx
+++ ex/tiled/assets/scifi.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.0" name="scifi" tilewidth="16" tileheight="16" tilecount="5" columns="5">
- <image source="../dat/gfx/greyzebes.png" width="80" height="16"/>
+ <image source="gfx/greyzebes.png" width="80" height="16"/>
 </tileset>
