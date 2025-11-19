0718d7fac1668f1f1c7d372a1216789a921452bf
Roger <roger.levy@gmail.com>
2025-11-19 09:18:53 -0500

    Fix load-project.bat

--- engineer/load-project.bat
+++ engineer/load-project.bat
@@ -1,9 +1,9 @@
 @echo off
-set "PATH=%~dp0\..\bin;%PATH%"
+set "PATH=%~dp0..\bin;%PATH%"
 if exist main.vfx (
     echo Loading project from %cd%
     engineer.exe ldp . %*
 ) else (
     echo No main.vfx found, starting Engineer normally
     engineer.exe %*
-)
+)go
