98813507bd8e413782a2a51d95d5c21e0c5cb9e0
Roger <roger.levy@gmail.com>
2025-11-28 15:30:24 -0500

    Update RPG example

--- ex/rpg/assets/map/objects.tsx
+++ ex/rpg/assets/map/objects.tsx
@@ -2,6 +2,6 @@
 <tileset version="1.10" tiledversion="1.11.0" name="objects" tilewidth="16" tileheight="16" tilecount="1" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image source="../../../../../../../pixel art/rpg/brainboy.gif" width="16" height="16"/>
+  <image source="brainboy.gif" width="16" height="16"/>
  </tile>
 </tileset>
